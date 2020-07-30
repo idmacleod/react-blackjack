82e17ff2045183931a76cd47bb4b9213618bba36
idmacleod <idmacleod@gmail.com>
2020-07-30 12:06:16 +0100

    Added state to top-level App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import GameForm from './components/GameForm';
 import GameDisplay from './containers/GameDisplay';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Blackjack</h1>
-      </header>
-      <GameForm />
-      <GameDisplay />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player1: null,
+      player2: null,
+      game: null
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>React Blackjack</h1>
+        </header>
+        <GameForm />
+        <GameDisplay />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -4,9 +4,9 @@ const GameForm = (props) => {
     return (
         <div className="game-form">
             <form>
-                <label for="p1">Player 1:</label>
+                <label htmlFor="p1">Player 1:</label>
                 <input type="text" name="p1" id="p1" />
-                <label for="p2">Player 2:</label>
+                <label htmlFor="p2">Player 2:</label>
                 <input type="text" name="p2" id="p2" />
                 <input type="submit" value="Start Game" />
             </form>
